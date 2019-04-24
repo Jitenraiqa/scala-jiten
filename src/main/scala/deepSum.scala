cfe32d4d6a138930856f423b32633d859005e480
Abby Henríquez Tejera <paradoja@gmail.com>
2019-04-24 11:14:13 +0200

    Changed `println` to `assert`
    This way we have simple tests - they shut up if all's good, complain
    otherwise.

--- src/main/scala/deepSum.scala
+++ src/main/scala/deepSum.scala
@@ -1,22 +1,16 @@
 def sum(list: List[Int]): Int = list match {
   case Nil => 0
   case head :: tail =>
-
     head + sum(tail)
 }
 
-println(sum(List(1,2,3,4)))
+assert(sum(List(1,2,3,4)) == 10)
 
 
 def deepsum(list: List[List[Int]]): Int = list match {
   case Nil => 0
   case head :: tail =>
-
     sum(head) + deepsum(tail)
-
 }
 
-
-
-
-println(deepsum(List(List(1,2), List(), List(), List(3), List(4,5))))
+assert(deepsum(List(List(1,2), List(), List(), List(3), List(4,5))) == 10)
